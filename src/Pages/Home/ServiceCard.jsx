d8cd9e8cb8e1adcb1d2066971408badeda6a78e2
AmjadChowdhury <amjadchowdhury247@gmail.com>
2025-01-06 19:33:51 +0600

    service section done

---
+++ src/Pages/Home/ServiceCard.jsx
@@ -0,0 +1,31 @@
+import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import { FaDollarSign } from "react-icons/fa";
+import PropTypes from "prop-types";
+
+const ServiceCard = ({ service }) => {
+  const { title, img, price } = service;
+  return (
+    <div className="card bg-base-100 shadow-xl">
+      <figure className="px-10 pt-10 h-full">
+        <img
+          src={img}
+          alt=""
+          className="rounded-xl"
+        />
+      </figure>
+      <div className="card-body items-center text-center">
+        <h2 className="card-title text-2xl font-extrabold uppercase">{title}</h2>
+        <div className="flex justify-between items-center text-xl font-bold uppercase text-yellow-500">
+            <p className="flex items-center mr-10">Price : <FaDollarSign></FaDollarSign> {price}</p>
+            <p><FaRegArrowAltCircleRight></FaRegArrowAltCircleRight></p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+ServiceCard.propTypes = {
+    service: PropTypes.object
+}
+
+export default ServiceCard;
---
+++ src/Pages/Home/Services.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+import ServiceCard from "./ServiceCard";
+
+
+const Services = () => {
+    const [services,setServices] = useState([])
+    useEffect(() => {
+        fetch("services.json")
+        .then(res => res.json())
+        .then(data => setServices(data))
+    },[])
+    return (
+        <div>
+            <div className="text-center">
+                <h1 className="text-5xl font-bold text-yellow-500">Services</h1>
+                <p className="text-4xl font-bold mt-4">Our services Area</p>
+                <p className="mt-2 mb-6 w-1/2 m-auto">the majority have suffered alteration in some form, by injected humour, or randomised words which do not look even slightly believable. </p>
+            </div>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-5">
+            {
+                services.map(service => <ServiceCard 
+                    key={service._id}
+                    service={service}></ServiceCard>)
+            }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
