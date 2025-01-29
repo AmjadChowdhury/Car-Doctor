9738975a2f7c9fbae15d50a93a6ff6476ef40ccb
AmjadChowdhury <amjadchowdhury247@gmail.com>
2025-01-29 12:59:44 +0600

    just one change

--- src/Pages/ServiceDeatils/Facilities.jsx
+++ src/Pages/ServiceDeatils/Facilities.jsx
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 const Facilities = ({fac}) => {
     const {name,details} = fac
     return (
-        <div className="p-4 border-t-4 border-b-2 border-t-orange-500 rounded-lg border-y-2 border-dotted">
+        <div className="p-4 border-t-4 border-b-2 border-t-orange-500 rounded-lg border-x-2 border-dotted">
             <h1 className="text-2xl font-bold">{name}</h1>
             <p>{details}</p>
         </div>
