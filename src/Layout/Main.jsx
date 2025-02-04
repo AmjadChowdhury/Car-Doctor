f7da708fc9f8dbe718cc16e853291744d7080047
AmjadChowdhury <amjadchowdhury247@gmail.com>
2025-02-04 10:56:17 +0600

    font added

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../Pages/Shared/Navbar";
 import Footer from "../Pages/Shared/Footer";
+import './Main.css'
 
 
 const Main = () => {
     return (
-        <div className="p-1 lg:p-2">
+        <div className="p-1 lg:p-2 font">
             <div className="max-w-6xl mx-auto">
                <Navbar></Navbar>
                <Outlet></Outlet>
